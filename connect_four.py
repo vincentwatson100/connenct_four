6f2c064f2db0be7fb85779dfb252cb76899e2982
Vincent <Vincewat5000@gmail.com>
2025-04-28 11:14:58 -0500

    git commit -m"changed it majorly"

--- connect_four.py
+++ connect_four.py
@@ -1,55 +1,93 @@
+#
+#
+#
+#
+#
+##import re
+#
+#class board:
+#    def __init__(self):
+#        self.grid = [[]]
+#        for row in range(6):
+#            self.grid.append([])
+#            for _ in range(7):
+#                self.grid[row].append('')
+#
+#    def print_board(self):
+#        for row in self.grid:
+#            print('|'.join(row))
+#            print('-'*13)
+#            print('0 1 2 3 4 5 6')
+#    def drop_disc(self, column, disc):
+#        if column < 0 or column >= 7 or self.grid[0][column] != ' ':
+#            return False
+#        for row in reversed(self.grid):
+#            if row[column] == ' ':
+#                row[column] = disc
+#                return True
+#            return False
+#        def check_winner(self):
+#            rows = [''.join0(row)for row in self.grid]
+#            cols = [''.join([self.grid[row][col]for row in range(6)])for col in range(7)]
+#            rising_diags = [''.join([self.grid[row-i][i] for i in range(max(row-5, 0), min(row+1, 7))]) for row in range(3, 11) if min(row+1, 7) - max(row-5, 0) >= 4]
+#            
+#            falling_diags = [''.join([self.grid[row+i][i] for i in range(max(-row, 0), min(6-row, 7))]) for row in range(-3, 6) if min(6-row, 7) - max(-row, 0) >= 4]
+#            for line in rows + cols + rising_diags + falling_diags:
+#                match = re.search(r'X{4}|O{4}})', line)
+#                if match:
+#                    return match.group()[0]
+#            return None
+#        def is_full(self):
+#            for col in range(7):
+#                if self.grid[0][col] == ' ':
+#                    return False
+#            return True
+
+import re
+
+class Board:
+    def __init__(self):
+        # self.grid = [[' ' for _ in range(7)] for _ in range(6)] # Simple way to do the same thing.
+        self.grid = [[]]
+        for row in range(6):
+            self.grid.append([])
+            for _ in range(7):
+                self.grid[row].append(' ')
+
+    def print_board(self):
+        for row in self.grid:
+            print('|'.join(row))
+            print('-' * 13)
+        print('0 1 2 3 4 5 6')
+
+    def drop_disc(self, column, disc):
+        if column < 0 or column >= 7 or self.grid[0][column] != ' ':
+            return False
+        for row in reversed(self.grid):
+            if row[column] == ' ':
+                row[column] = disc
+                return True
+        return False
+
+    def check_winner(self):
+        rows = [''.join(row) for row in self.grid]
+        cols = [''.join([self.grid[row][col] for row in range(6)]) for col in range(7)]
+        rising_diags = [''.join([self.grid[row-i][i] for i in range(max(row-5, 0), min(row+1, 7))]) for row in range(3, 11) if min(row+1, 7) - max(row-5, 0) >= 4]
+        falling_diags = [''.join([self.grid[row+i][i] for i in range(max(-row, 0), min(6-row, 7))]) for row in range(-3, 6) if min(6-row, 7) - max(-row, 0) >= 4]
+
+        for line in rows + cols + rising_diags + falling_diags:
+            match = re.search(r'(X{4}|O{4})', line)
+            if match:
+                return match.group()[0]
+        return None
+    
+    def is_full(self):
+        for col in range(7):
+            if self.grid[0][col] == ' ':
+                return False
+        return True
 
 
-
-
-
-
-class board:
-    def __init__(self, grid, print_board, drop_disc):
-        self.grid = grid
-        self.print_board = print_board 
-        self.drop_disc = drop_disc
-
-
-
-
-
-num = ('0 1 2 3 4 5 6')
-x = ('x')
-o = ('o')
-disc= input(f'are you {x} or {o}:')
-print(disc)
-
-for _ in range(6):
-
-
-    print('| | | | | | | |')
-    print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
-
-
-print(num)
-column = input('column number:')
-
-print(column)
-
-for _ in range(1,6):
-    if column == '0':
-        print('True')
-    elif column == '1':
-        print('True')
-    elif column == '2':
-        print('True')
-    elif column == '3':
-        print('True')
-    elif column == '4':
-        print('True')
-    elif column == '5':
-        print('True')
-    elif column == '6':
-        print('True')
-
-    else:
-        input('False, try again')
 
   
 
